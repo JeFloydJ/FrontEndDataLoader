ba5ce4315a05dc5d72e7b250b1bc708e253c9e16
Juan Esteban Floyd <juan.floyd@veevart.com>
2024-05-30 16:55:30 -0500

    test, local files, sent to salesforce

--- static/scripts/formAction.js
+++
@@ -1,8 +0,0 @@
-function setAction(action) {
-    const form = document.getElementById('upload-form');
-    if (action === 'upload') {
-        form.action = '/upload';
-    } else if (action === 'delete') {
-        form.action = '/delete';
-    }
-}
